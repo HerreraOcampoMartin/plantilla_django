c17085457becd7914978f8b2afc339a81be262cb
Martín Herrera Ocampo <herreraocampomartin@gmail.com>
2025-12-05 15:02:02 -0300

    Poner que el orden por defecto para ModeloOrdenable sea 0

--- core/modelos/modelos_abstractos.py
+++ core/modelos/modelos_abstractos.py
@@ -11,7 +11,7 @@ class ModeloEliminacionLogica(models.Model):
 
 class ModeloOrdenable(models.Model):
     """ Modelo base que ofrece la posibilidad de hacer modelos ordenables """
-    orden = models.IntegerField()
+    orden = models.IntegerField(default=0)
 
     # Aplica el manager al modelo abstracto
     objects = OrdenableManager()
